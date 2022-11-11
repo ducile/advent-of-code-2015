517d266403e55dd05362c5e8c212e1f3478f9d68
Duci Le <ducanh.le@hotmail.de>
2022-11-11 21:12:59 +0100

    Solution Day 3 Part 2

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -27,5 +27,46 @@ fun main() {
 
     }
 
-    println(touchedPoints.count())
+    println("Part 1: " + touchedPoints.count())
+
+    // Part 2
+
+    var santaX = 0
+    var santaY = 0
+
+    var roboX = 0
+    var roboY = 0
+
+    var deliveredPoints = mutableListOf(0 to 0)
+
+    for (i in input.indices) {
+        if (i % 2 == 0) {
+            when (input[i]) {
+                '>' -> santaX += 1
+                '^' -> santaY += 1
+                '<' -> santaX -= 1
+                else -> santaY -= 1
+            }
+            val currentPoint = santaX to santaY
+
+            if (! deliveredPoints.contains(currentPoint)) {
+                deliveredPoints.add(currentPoint)
+            }
+        } else {
+            when (input[i]) {
+                '>' -> roboX += 1
+                '^' -> roboY += 1
+                '<' -> roboX -= 1
+                else -> roboY -= 1
+            }
+
+            val currentPoint = roboX to roboY
+
+            if (! deliveredPoints.contains(currentPoint)) {
+                deliveredPoints.add(currentPoint)
+            }
+        }
+    }
+
+    println("Part 2: " + deliveredPoints.count())
 }
