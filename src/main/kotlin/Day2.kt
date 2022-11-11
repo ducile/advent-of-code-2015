e26b4bcb824edb41da1f9122f2f95494dba3acea
Duci Le <ducanh.le@hotmail.de>
2022-11-11 16:51:41 +0100

    Day 2 Part 2 solution

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.util.Collections.sort
 
 fun main() {
     val input = File("data/day2_packages").readText()
@@ -6,8 +7,13 @@ fun main() {
     val list = input.split("\n").map { it.trim().split("x") }
 
     var totalPaperNeeded = 0
+    var totalRibbonLength = 0
 
     for (box in list) {
+
+        val result = box.map { it.toInt() }.sorted()
+
+        // Part 1
         val l = box[0].toInt()
         val w = box[1].toInt()
         val h = box[2].toInt()
@@ -23,7 +29,16 @@ fun main() {
         val paperNeeded = surface + min
 
         totalPaperNeeded += paperNeeded
+
+        // Part 2
+        val min1 = result[0]
+        val min2 = result[1]
+
+        val ribbonLength = result.reduce { acc, element -> acc * element }
+
+        totalRibbonLength += ribbonLength + (min1 * 2 + min2 * 2)
     }
 
     println(totalPaperNeeded)
+    println(totalRibbonLength)
 }
