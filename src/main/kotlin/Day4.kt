22dd7b4e7468bd430bbbbe5bbbd26c073186261f
Duci Le <ducanh.le@hotmail.de>
2022-11-13 20:22:44 +0100

    Solution Day 4 part 1

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,33 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+
+
+fun md5(input:String): String {
+    val md = MessageDigest.getInstance("MD5")
+
+    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+}
+
+fun main() {
+    val input = "bgvyzdsv"
+    var count = 0
+    var searching = true
+
+    val testHash = md5("abcdef609043")
+
+    if (testHash.substring(0, 5) == "00000") {
+        println(testHash)
+    }
+
+    while(searching) {
+        val message = input + count
+        val hash = md5(message)
+
+        if (hash.substring(0, 5) == "00000") {
+            searching = false
+            println(count)
+        }
+
+        count ++
+    }
+}
